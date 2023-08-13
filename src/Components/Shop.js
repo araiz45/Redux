f8ad35382b2852fd3dcacdec8651dfd005440094
araiz45 <araizzahid45@gmail.com>
2023-08-13 16:29:36 +0500

    video 74 completed

---
+++ src/Components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+        <h2>Buy Addidous Shoes in 50 RS</h2>
+        <button type="button" class="btn btn-primary mx-2">-</button>
+            Add item to cart
+        <button type="button" class="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
