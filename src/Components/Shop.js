33f54e1413e9347da90f16eebf1ba12efebaed8e
araiz45 <araizzahid45@gmail.com>
2023-08-14 01:08:41 +0500

    completed playlist

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../State/index'
+import { useSelector } from 'react-redux'
 
 function Shop() {
   const dispatch = useDispatch();
+  const amount = useSelector(state => state.amount);
   const {withdrawMoney, depositMoney} = bindActionCreators(actionCreators, dispatch)
   return (
     <div>
         <h2>Deposit / Withdraw Money</h2>
         <button type="button" class="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
-            Update Balance
+            Update Balance : {amount}
         <button type="button" class="btn btn-primary mx-2" onClick={()=>{depositMoney(100)}}>+</button>
     </div>
   )
